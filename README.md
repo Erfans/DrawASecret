0cf997d16b4c8e09bd74edddd1678774b2f7274c
Erfans <e.shamabadi@gmail.com>
2014-03-22 00:04:33 +0430

    Return PicturePassword object on initialize.

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 PicturePassword
 ===============
 
-jQuery extension to have picture passwords on websites
-
-
-This extension relates an image to a input element. Thouched or mouse dragged patterns convert to a string of numbers and set as element's value.
-
+jQuery extension to make "picture passwords" for websites
--- picturePassword.js
+++ picturePassword.js
@@ -1,83 +1,121 @@
 /* 
     Document   : picturePassword
-    Created on : Dec 26, 2013
-    Author     : Erfan - www.erfan.shamabadi.com
+    Author     : erfan.shamabadi.com
     Description:
-        jQuery extension to have picture password on websites
+        jQuery extension to make picture password for website
  */
-(function($) {
-    $.fn.picturePassword = function(passwordElm,options) {
-        if(passwordElm == undefined){
-            console.error("Password Elememt couldn't be null!");
-            return;
-        }
-        
-        passwordElm = $(passwordElm);
-        
-        var pathString = "";        
-        var isTouching = false;        
-        var lastPositionX = -1,
-        lastPositionY = -1,
-        currentPositionX = -1,
-        currentPositionY = -1;
-        
-        var imgNum = 0;
-        var settings = $.extend({
-            rowsNb : 15, // Number of rows
-            columnsNb : 15 // Number of columns
-        }, options );
-        
-        this.clearPassword = function(){
-            pathString = "";
-            passwordElm.val(pathString);
+(function($, doc) {
+    
+    "use strict";
+    
+    var PicturePassword = function($img,$input,opts){
+      
+
+        var options ={
+            rows :15,
+            columns:15
         };
+
+
+        this.$img = $img;
+        this.$input = $input;
         
-        return this.each(function() {                  
+        this.pathString = "";
+        this.isTouching = false;        
+        this.lastPosition = {
+            x:-1,
+            y:-1
+        };     
+        
+        this.init($.extend(options, opts ));
+    };
+    
+    PicturePassword.prototype.init = function(options){
+        
+        var self = this;
+        var imgNum = 0;
+        var $img = self.$img;
+        
+        $img.each(function() {                  
+
+            var img = this;
             
-            this.columnWidth = Math.floor(this.width / settings.columnsNb);
-            this.rowHeight = Math.floor(this.height / settings.rowsNb);            
-            this.imgNum = imgNum++;           
-                               
-            var makeString = function(data, offset, columnWidth, rowHeight, imgNum){
-                currentPositionX = Math.floor((data.pageX - offset.left) / columnWidth);
-                currentPositionY = Math.floor((data.pageY - offset.top) / rowHeight);
-                
-                if(lastPositionX != currentPositionX || lastPositionY != currentPositionY){
-                    pathString += imgNum.toString() + currentPositionX.toString() + currentPositionY.toString();
-                    passwordElm.val(pathString);
-                    lastPositionX = currentPositionX;
-                    lastPositionY = currentPositionY;
-                }
+            img.picturePassword ={
+                num :imgNum++,
+                cw:Math.floor(img.width / options.columns),
+                rh:Math.floor(img.height / options.rows)
             };
-            
-            $(this).on("touchstart mousedown", function(e){
-                e.preventDefault();
-                var data = e.originalEvent.touches ? e.originalEvent.touches[0] : e;                
-                makeString(data, $(this).offset(), this.columnWidth, this.rowHeight, this.imgNum);
-                isTouching = true;
-            });
-            $(this).on("touchmove mousemove", function(e){
-                e.preventDefault();
-                var data = e.originalEvent.touches ? e.originalEvent.touches[0] : e;                
-                if(isTouching){
-                    makeString(data, $(this).offset(), this.columnWidth, this.rowHeight, this.imgNum);
-                }
-            });
-            $(this).on("touchend mouseup", function(e){
-                e.preventDefault();                        
-                isTouching = false;                                
-            });
-        
-            $(document).on("touchend mouseup",function(){
-                lastPositionX = -1;
-                lastPositionY = -1;
-                isTouching = false;
-            })
-        
-            $(this).on('dragstart', function(event) {
-                event.preventDefault();
-            });        
         });
+        
+        $img.on("touchstart mousedown", function(event){
+            event.preventDefault();  
+            var img = this;
+            var pp = img.picturePassword;
+            self.makeString(event, $(img).offset(), pp.cw, pp.rh, pp.num);
+            self.isTouching = true;
+        });
+            
+        $img.on("touchmove mousemove", function(event){
+            event.preventDefault();                
+            if(self.isTouching){                
+                var img = this;
+                var pp = img.picturePassword;
+                self.makeString(event, $(img).offset(), pp.cw, pp.rh, pp.num);
+            }
+        });
+            
+        $img.on("touchend mouseup", function(e){
+            e.preventDefault();                        
+            self.isTouching = false;                                
+        });
+        
+        $(doc).on("touchend mouseup",function(){
+            self.lastPosition = {
+                x:-1,
+                y:-1
+            };                
+            self.isTouching = false;
+        });
+        
+        $img.on('dragstart', function(event) {
+            event.preventDefault();
+        });        
+        
+    };
+    
+    PicturePassword.prototype.makeString = function(event, offset, columnWidth, rowHeight, imgNum){
+        var self = this;
+        var lastPosition = self.lastPosition;
+        var data = event.originalEvent.touches ? event.originalEvent.touches[0] : event;
+        
+        var currentPositionX = Math.floor((data.pageX - offset.left) / columnWidth);
+        var currentPositionY = Math.floor((data.pageY - offset.top) / rowHeight);
+                
+        if( lastPosition.x != currentPositionX || lastPosition.y != currentPositionY){
+            self.pathString += ''+imgNum+ currentPositionX+ currentPositionY;
+            
+            if(self.$input){
+                self.$input.val(self.pathString);
+            }
+            
+            self.lastPosition = {
+                x:currentPositionX , 
+                y:currentPositionY
+            };            
+        }
+    };
+    
+    PicturePassword.prototype.clearPassword = function(){
+        var self = this;
+        self.pathString = "";
+        if(self.$input){
+            self.$input.val("");
+        }
+    }
+    
+    $.fn.picturePassword = function(inputSelector,options) {
+        var $input = inputSelector? $(inputSelector):null;
+        return new PicturePassword(this,$input,options);
     }
 
-}(jQuery));
+})(jQuery, document);
